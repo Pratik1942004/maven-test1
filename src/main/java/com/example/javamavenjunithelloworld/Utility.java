78e508e2ec8ed978f8d26bd2748414e0b5656d55
pratik_bansode <pratikbansode1904@gmail.com>
2024-09-16 14:31:01 +0530

    PRATIKB0001-Wrote the code for printing 1 to 10

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,14 @@
+class Utility{
+         
+          public static void main(String[] args){
+                    Utility obj=new Utility();
+           }
+
+         public pratik(){
+                     System.out.println("I am Pratik.");
+                     System.out.println("Program to print from 1 to 10");
+                     for(int i=1;i<=10;i++){
+                          System.out.println(i);
+                        }
+                }
+}
